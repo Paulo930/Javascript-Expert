fd4601ed919cdcd27f3ed25d10aa2a965b75fdce
Paulo930 <pauloedu531@gmail.com>
2022-03-30 17:35:54 -0300

    '...'

--- Modulo-04/aula01-generators-iterators/gererators-iterators.js
+++ Modulo-04/aula01-generators-iterators/gererators-iterators.js
@@ -29,13 +29,32 @@ assert.deepStrictEqual([...main()], ['Hello', '-', 'World', 200]);
 
 // ---- async iterators
 const { readFile, stat, readdir } = require('fs/promises');
-
 function* promisified() {
   yield readFile(__filename);
   yield Promise.resolve('Hey Dude');
 }
 
-Promise.all([...promisified()]).then((results) => {
-  console.log('promisified', results);
-});
-// console.log('promisified', [...promisified()]);
+async function* systemInfo() {
+  const file = await readFile(__filename);
+  yield { file: file.toString() };
+
+  const { size } = await stat(__filename);
+  yield { size };
+
+  const dir = await readdir(__dirname);
+  yield { dir };
+}
+
+// Promise.all([...promisified()]).then((results) => {
+//   console.log('promisified', results);
+// });
+// (async () => {
+//   for await (const item of promisified()) {
+//     console.log('for await', item.toString());
+//   }
+// })();
+(async () => {
+  for await (const item of systemInfo()) {
+    console.log('systemInfo', item);
+  }
+})();
---
+++ Modulo-04/aula03-map-e-weakmap/map-e-weakmap.js
@@ -0,0 +1,98 @@
+const assert = require('assert');
+const myMap = new Map();
+
+// podem ter qualquer coisa como chave
+myMap
+  .set(1, 'one')
+  .set('Erick', { text: 'two' })
+  .set(true, () => 'hello');
+
+// usando um construtor
+const myMapWithConstructor = new Map([
+  ['1', 'str1'],
+  [1, 'num1'],
+  [true, 'bool1'],
+]);
+
+// console.log('myMap', myMap);
+// console.log('myMap.get(1)', myMap.get(1));
+assert.deepStrictEqual(myMap.get(1), 'one');
+assert.deepStrictEqual(myMap.get('Erick'), { text: 'two' });
+assert.deepStrictEqual(myMap.get(true)(), 'hello');
+
+// Em Objects a chave só pode ser string ou symbol (number é coerido a string)
+const onlyRefenceWorks = { id: 1 };
+myMap.set(onlyRefenceWorks, { name: 'ErickWendel' });
+
+assert.deepStrictEqual(myMap.get({ id: 1 }), undefined);
+assert.deepStrictEqual(myMap.get(onlyRefenceWorks), { name: 'ErickWendel' });
+
+// utilitários
+// - No Object seria Object.keys({a: 1}).length
+assert.deepStrictEqual(myMap.size, 4);
+
+//  para verificar se um item existe no objeto
+// item.key = se não existe = undefined
+// if() = coerção implicita para boolean e retorna false
+// O jeito certo em Object é ({ name: 'Erick' }).hasOwnProperty('name')
+assert.ok(myMap.has(onlyRefenceWorks));
+
+// para remover um item do objeto
+// delete item.id
+// imperformático para o Javascript
+assert.ok(myMap.delete(onlyRefenceWorks));
+
+// Não dá para iterar em Objects diretamente
+// tem que transformar com o Object.entries(item)
+assert.deepStrictEqual(
+  JSON.stringify([...myMap]),
+  JSON.stringify([
+    [1, 'one'],
+    ['Erick', { text: 'two' }],
+    [true, () => {}],
+  ]),
+);
+
+// for (const [key, value] of myMap) {
+//   console.log({ key, value });
+// }
+
+// Object é inseguro, pois dependendo do nome da chave, pode substituir algum comportamento padrão
+// ({ }).toString() === '[object Object]'
+// ({ toString: () => 'Hey' }).toString() === 'Hey'
+
+// qualquer chave pode colidir, com as propriedades heradas do objecto, como
+// constructor, toString, valueOf e etc
+
+const actor = {
+  name: 'Xuxa de Silva',
+  toString: 'Quee',
+};
+
+// não tem restrição de nome de chave
+myMap.set(actor);
+
+assert.ok(myMap.has(actor));
+assert.throws(() => myMap.get(actor).toString, TypeError);
+
+// Não da para limpar um Obj sem reassina-lo
+myMap.clear();
+assert.deepStrictEqual([...myMap.keys()], []);
+
+// --- WeakMap
+
+// Pode ser coletado após perder as referencias
+// usado em casos beeem específicos
+
+// tem a meioria dos benefícios do Map
+// MAIS: não é iterável
+// Só chaves de referencia e que você já conheça
+// mais leve e preve leak de memoria, pq depois que as instancias saem da memoria, tudo é limpo
+
+const weakMap = new WeakMap();
+const hero = { name: 'Flash' };
+
+// weakMap.set(hero);
+// weakMap.get(hero);
+// weakMap.delete(hero);
+// weakMap.has(hero);
